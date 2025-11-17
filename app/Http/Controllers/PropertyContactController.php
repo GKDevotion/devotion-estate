f9adf7de22d622a444b31317ff2cd9e2e19f605e
GKDevotion <gk@devotiontech.io>
2025-11-17 19:50:34 +0530

    update try catch

--- app/Http/Controllers/PropertyContactController.php
+++ app/Http/Controllers/PropertyContactController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Properties;
 use App\Models\PropertyContact;
 use App\Models\Review;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
@@ -43,42 +44,46 @@ class PropertyContactController extends Controller
         ];
 
 
-        // // SEND MAIL
-        // Mail::send('frontend.emails.contactSellerMail', $data, function($message) use ($data){
-        //     $message->to('admin@devotionestate.com')
-        //             ->subject('New Contact Seller Message');
-        // });
+        try{
+            // SEND MAIL
+            Mail::send('frontend.emails.contactSellerMail', $data, function($message) use ($data){
+                $message->to('admin@devotionestate.com')
+                        ->subject('New Contact Seller Message');
+            });
+        } catch( Exception $e ){
 
-        $to = "admin@devotionestate.com";
-        $subject = "New Contact Seller Message";
-
-        $message = "
-        <html>
-        <body>
-        <h3>New Contact Message</h3>
-        <p><strong>Property:</strong> ".$data['propertyname']." (".$data['propertyid'].")</p>
-        <p><strong>Name:</strong> ".$data['name']."</p>
-        <p><strong>Mobile:</strong> ".$data['mobile']."</p>
-        <p><strong>Email:</strong> ".$data['email']."</p>
-        <p><strong>Message:</strong> ". $data['msg']."</p>
-        </body>
-        </html>
-        ";
-
-        $headers  = "MIME-Version: 1.0" . "\r\n";
-        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-        $headers .= "From: Devotion Estate <admin@devotionestate.com>" . "\r\n";
-
-        $emailmsg = "";
-        if(mail($to, $subject, $message, $headers)){
-            $emailmsg = "Email sent successfully";
-        }else{
-            $emailmsg = "Email sending failed";
         }
+
+        // $to = "admin@devotionestate.com";
+        // $subject = "New Contact Seller Message";
+
+        // $message = "
+        // <html>
+        // <body>
+        // <h3>New Contact Message</h3>
+        // <p><strong>Property:</strong> ".$data['propertyname']." (".$data['propertyid'].")</p>
+        // <p><strong>Name:</strong> ".$data['name']."</p>
+        // <p><strong>Mobile:</strong> ".$data['mobile']."</p>
+        // <p><strong>Email:</strong> ".$data['email']."</p>
+        // <p><strong>Message:</strong> ". $data['msg']."</p>
+        // </body>
+        // </html>
+        // ";
+
+        // $headers  = "MIME-Version: 1.0" . "\r\n";
+        // $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+        // $headers .= "From: Devotion Estate <admin@devotionestate.com>" . "\r\n";
+
+        // $emailmsg = "";
+        // if(mail($to, $subject, $message, $headers)){
+        //     $emailmsg = "Email sent successfully";
+        // }else{
+        //     $emailmsg = "Email sending failed";
+        // }
 
         return response()->json([
             'success' => true,
-            'message' => 'Your message has been sent successfully!'.$emailmsg
+            'message' => 'Your message has been sent successfully!'
         ]);
 
         // return back()->with('success', 'Your Contact Data has been submitted successfully!');
