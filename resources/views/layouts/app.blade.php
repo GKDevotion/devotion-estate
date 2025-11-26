b158921ff235a40bd9e302157daa7a6444b9b9e5
GKDevotion <gk@devotiontech.io>
2025-11-26 11:04:08 +0530

    add chat widget

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -356,7 +356,6 @@
             });
         });
 
-
         $('#contactsellerForm').on('submit', function(e) {
             e.preventDefault();
 
@@ -448,7 +447,22 @@
                 },
             });
         });
+
     </script>
+
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+        (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/6673f572dd590416e25803a3/1i0qfdht3';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+        })();
+    </script>
+    <!--End of Tawk.to Script-->
 </body>
 
 </html>
