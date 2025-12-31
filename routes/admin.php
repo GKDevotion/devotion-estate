f0208fdb25c480278752e6559f9eb43a0e67f274
hardik prajapati <hardikprajapati8104@gmail.com>
2025-12-31 18:12:06 +0530

    updated

--- routes/admin.php
+++ routes/admin.php
@@ -109,8 +109,7 @@ Route::prefix('admin')->group(function () {
     // Information Model
     Route::post('admin/properties/update-information', [PropertiesController::class, 'updateInformation'])->name('admin.properties.updateInformation');
     Route::get('properties/{id}/information',[PropertiesController::class, 'getInformation'])->name('admin.properties.getInformation');
-Route::post('admin/properties/update-variants', [PropertiesController::class, 'updateVariants'])
-    ->name('admin.properties.updateVariants');
+    Route::post('admin/properties/update-variants', [PropertiesController::class, 'updateVariants'])->name('admin.properties.updateVariants');
 
 
     Route::resource('developer', 'Backend\DeveloperController', ['names' => 'admin.developer']);
